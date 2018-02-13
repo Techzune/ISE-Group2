967659c29aea4e8ed94b7469876d213be1960c15
Techzune <techzune@operontech.com>
2018-02-12 20:08:59 -0600

    Added some starter code and a header for fun

--- SWEG2Application.py
+++ SWEG2Application.py
@@ -1,3 +1,29 @@
 # title: SWEG2Application
 # author: Avan Patel, Kohler Smallwood, Azlin Reed, Jordan Stremming, Steven Huynh, Zach Butterbaugh
 # purpose: Primary application file; hub of application
+import time
+
+
+# initialize the program
+def init():
+    # print out header
+    print("====================================================",
+          "| SWEG2 (SOFTWARE ENGINEERING GROUP 2) LAB PROJECT |",
+          "====================================================",
+          "|                   <<[THE TEAM]>>                 |",
+          "|              Avan Patel    Kohler Smallwood      |",
+          "|              Azlin Reed    Jordan Stremming      |",
+          "|            Steven Huynh    Zach Butterbaugh      |",
+          "====================================================",
+          "",
+          sep="\n")
+
+    # print a hello world statement
+    print("Hello world!")
+
+    # wait 3 seconds
+    time.sleep(3)
+
+
+# run the init function
+init()
