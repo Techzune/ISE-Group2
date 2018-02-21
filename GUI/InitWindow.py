7f95bac02a7c2145fb108b383877c77ef0fe3107
Jordan Stremming <jstremming@operontech.com>
2018-02-21 10:00:11 -0600

    Implemented basic application open with InitWindow

--- GUI/InitWindow.py
+++ GUI/InitWindow.py
@@ -1,4 +1,36 @@
 # title: InitWindow
 # author: Avan Patel, Kohler Smallwood, Azlin Reed, Jordan Stremming, Steven Huynh, Zach Butterbaugh
 # purpose: Backbone of init window; a configuration window to define parameters for software
+from PyQt5 import QtCore
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QMainWindow, QGridLayout, QWidget, QLabel
 
+
+# inherits QMainWindow (a basic window)
+class InitWindow(QMainWindow):
+
+    def __init__(self, main_app):
+        # run the init of QMainWindow
+        super().__init__()
+
+        # save a reference to the main_app
+        self.main_application = main_app
+
+        # set a fixed size, prevents resizing
+        self.setFixedSize(640, 480)
+
+        # set the title
+        self.setWindowTitle("Initialization Window")
+
+        # set the main widget of the window
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
+
+        # create a grid layout for the central widgget
+        gridLayout = QGridLayout(self)
+        centralWidget.setLayout(gridLayout)
+
+        # create a label centered with "Hello World"
+        title = QLabel("Hello World", self)
+        title.setAlignment(QtCore.Qt.AlignCenter)
+        gridLayout.addWidget(title)
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 # title: SWEG2Application
 # author: Avan Patel, Kohler Smallwood, Azlin Reed, Jordan Stremming, Steven Huynh, Zach Butterbaugh
 # purpose: Primary application file; hub of application
-import time
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from GUI.InitWindow import InitWindow
 
 
-# initialize the program
-def init():
-    # print out header
+# MainAppliction class
+class MainApplication:
+
+    # runs on MainApplication creation
+    def __init__(self):
+        # create the application
+        app = QApplication([])
+
+        # start the application loop (this prevents the program from exiting instantly)
+        sys.exit(app.exec_())
+
+    # shows the initialization window
+    def show_init_window(self):
+        # create the init window and show
+        init_window = InitWindow(self)
+        init_window.show()
+
+    # starts the algorithm
+    # acts as a callback from InitWindow or from command line
+    def start_algorithm(self, **kwargs):
+        pass
+
+
+# PROGRAM START #
+# check if this is the main file (if the user opened this file first)
+if __name__ == "__main__":
+    # print a header
     print("====================================================",
           "| SWEG2 (SOFTWARE ENGINEERING GROUP 2) LAB PROJECT |",
           "====================================================",
@@ -18,12 +44,8 @@ def init():
           "",
           sep="\n")
 
-    # print a hello world statement
-    print("Hello world!")
+    # create the main application (python does not use "new")
+    main = MainApplication()
 
-    # wait 3 seconds
-    time.sleep(3)
-
-
-# run the init function
-init()
+    if len(sys.argv) == 0:
+        main.show_init_window()
