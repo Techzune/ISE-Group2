3a543574dedf06eaed315c72519589357ea23658
Techzune <techzune@operontech.com>
2018-02-22 18:18:37 -0600

    Implemented InitWindow UI and events

--- GUI/InitWindow.py
+++ GUI/InitWindow.py
@@ -1,36 +1,74 @@
 # title: InitWindow
 # author: Avan Patel, Kohler Smallwood, Azlin Reed, Jordan Stremming, Steven Huynh, Zach Butterbaugh
 # purpose: Backbone of init window; a configuration window to define parameters for software
-from PyQt5 import QtCore
-from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QMainWindow, QGridLayout, QWidget, QLabel
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.uic import loadUi
 
 
-# inherits QMainWindow (a basic window)
 class InitWindow(QMainWindow):
+    """
+    Initialization Window, inherits from QMainWindow (a basic window)
+    """
 
     def __init__(self, main_app):
+        """
+        Initialization of the Initializaton Window
+
+        :param main_app: a reference to main.py (MainApplication)
+        """
+
         # run the init of QMainWindow
         super().__init__()
 
         # save a reference to the main_app
         self.main_application = main_app
 
-        # set a fixed size, prevents resizing
-        self.setFixedSize(640, 480)
+        # load .ui file for window
+        loadUi('GUI/InitWindow.ui', self)
 
         # set the title
         self.setWindowTitle("Initialization Window")
 
-        # set the main widget of the window
-        centralWidget = QWidget(self)
-        self.setCentralWidget(centralWidget)
+        # define the algorithms in the comboBox
+        self.cbox_algorithm.addItems(["BubbleSort", "CountingSort", "InsertionSort", "MergeSort", "QuickSort"])
 
-        # create a grid layout for the central widgget
-        gridLayout = QGridLayout(self)
-        centralWidget.setLayout(gridLayout)
+        # define the sources for numbers
+        self.cbox_source_select.addItems(["File with numbers", "Random list of size N", "Manual number input"])
 
-        # create a label centered with "Hello World"
-        title = QLabel("Hello World", self)
-        title.setAlignment(QtCore.Qt.AlignCenter)
-        gridLayout.addWidget(title)
+        # add event listener to button click
+        self.btn_run.clicked.connect(self.on_run_clicked)
+
+        # add event listener to combobox
+        self.cbox_source_select.currentIndexChanged.connect(self.on_source_select_changed)
+
+    @pyqtSlot()
+    def on_run_clicked(self):
+        """
+        EVENT: click for "Run Algorithm" button
+        """
+
+        # TODO: implement return of data to MainApplication
+        print("let's play ball!")
+
+    @pyqtSlot()
+    def on_source_select_changed(self):
+        """
+        EVENT: selection change for "Source Select" comboBox
+        """
+
+        # get the selected index
+        index = self.cbox_source_select.currentIndex()
+
+        if index == 0:
+            # "File with numbers" is selected
+            self.lbl_source.setText("Path to numbers file (*.txt):")
+            self.btn_browse.setEnabled(True)
+        elif index == 1:
+            # "Random list of size N" is selected
+            self.lbl_source.setText("Size of N (int):")
+            self.btn_browse.setEnabled(False)
+        elif index == 2:
+            # "Manual number input" is selected
+            self.lbl_source.setText("List of numbers (separated by commas):")
+            self.btn_browse.setEnabled(False)
