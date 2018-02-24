e0e58d6506af0cf61b925168af65231dd82bd669
ampatel013 <ampatel013@yahoo.com>
2018-02-23 19:53:31 -0600

    bubble

--- Algorithm/BubbleSort.py
+++ Algorithm/BubbleSort.py
@@ -4,36 +4,42 @@
 # At the end of 1st run, the last element in the array will be largest..
 
 
-def sort(element):
-    n = len(element)
-    value = True
-    for each in range(n):                   # Go Through the Element in Array
-        for i in range(0, n-each-1):        # After first run last element is sorted so you want to skip going that far
-            if element[i] > element[i+1]:   # If i > i+1 then swap it
-                element[i], element[i+1] = element[i+1], element[i]
-                value = False
-        # If array already sorted then Break
-        if value is True:
-            print("Array already sorted")
-            break
-    return element
-
-element = [12, 5, 2, 6, 7, 3]               # Random Testing Numbers
-
-array = []                                  # Taking an input from File
-filename = input("What is the name of input file? ")
-with open(filename) as f:
-    for line in f:
-        array.append(line)                  # Appending each element of file to List as string
+from Algorithm.Algorithm import Algorithm
 
 
-array = list(map(int, array))               # Converting list to string
-sort(array)                                 # Calling Function
+class BubbleSort(Algorithm):
+
+    def sort(self, element):
+        n = len(element)
+        value = True
+        for each in range(n):                   # Go Through the Element in Array
+            for i in range(0, n-each-1):        # After first run last element is sorted so you want to skip going that far
+                if element[i] > element[i+1]:   # If i > i+1 then swap it
+                    element[i], element[i+1] = element[i+1], element[i]
+                    value = False
+            # If array already sorted then Break
+            if value is True:
+                print("Array already sorted")
+                break
+        return element
 
 
-# Print out Element in Array
-for each in range(len(array)):
-    print(array[each])
 
+# element = [12, 5, 2, 6, 7, 3]               # Random Testing Numbers
+# array = []                                  # Taking an input from File
+# filename = input("What is the name of input file? ")
+# with open(filename) as f:
+#     for line in f:
+#         array.append(line)                  # Appending each element of file to List as string
+#
+#
+# array = list(map(int, array))               # Converting list to string
+# sort(array)                                 # Calling Function
+#
+#
+# # Print out Element in Array
+# for each in range(len(array)):
+#     print(array[each])
+#
 
 
