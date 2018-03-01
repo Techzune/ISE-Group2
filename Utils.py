88221d7fac6bf0da586fc9fa3d997f92233f8ab8
Jordan Stremming <jstremming@operontech.com>
2018-03-01 15:20:09 -0600

    Added file to number list converter

--- Utils.py
+++ Utils.py
@@ -17,3 +17,34 @@ def isInt(val):
     except ValueError:
         # conversion failed
         return False
+
+
+def file_to_nums(file_path):
+    """
+    Converts a file of numbers into a list of numbers.
+    Assumes numbers are separated by new-lines.
+
+    :param file_path: the path to the file
+    :return: the list of numbers
+    """
+
+    # create an empty list
+    num_list = []
+
+    # open the path as "file" -- this automatically closes the file
+    with open(file_path) as file:
+        # get each line in the file
+        for line in file:
+            # strip the line of "\n"
+            line = line.rstrip()
+
+            # check if the line is an integer
+            if isInt(line):
+                # if int, add to list
+                num_list.append(int(line))
+            else:
+                # otherwise, skip the line
+                print("WARNING: line skipped in file, not integer:", line)
+
+    # return the list
+    return num_list
