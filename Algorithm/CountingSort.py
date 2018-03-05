ea93725c47fb0de982137e74938429bdc3400927
Jordan Stremming <jstremming@operontech.com>
2018-03-05 09:25:32 -0600

    Added blank classes to those missing classes

--- Algorithm/CountingSort.py
+++ Algorithm/CountingSort.py
@@ -1,4 +1,10 @@
 # title: CountingSort
 # author: Avan Patel, Kohler Smallwood, Azlin Reed, Jordan Stremming, Steven Huynh, Zach Butterbaugh
 # purpose: algorithm;
+from Algorithm.Algorithm import Algorithm
 
+
+class CountingSort(Algorithm):
+
+    def sort(self, num_list):
+        pass
--- Algorithm/InsertionSort.py
+++ Algorithm/InsertionSort.py
@@ -1,4 +1,10 @@
 # title: InsertionSort
 # author: Avan Patel, Kohler Smallwood, Azlin Reed, Jordan Stremming, Steven Huynh, Zach Butterbaugh
 # purpose: algorithm;
+from Algorithm.Algorithm import Algorithm
 
+
+class InsertionSort(Algorithm):
+
+    def sort(self, num_list):
+        pass
