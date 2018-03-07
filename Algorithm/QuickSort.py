cb150d1334fcea91b8719a9b52f605ab127f33fa
Steven Huynh <aprentice@operontech.com>
2018-03-06 19:18:03 -0600

    Adding QuickSort. Hopefully it works

--- Algorithm/QuickSort.py
+++ Algorithm/QuickSort.py
@@ -7,4 +7,31 @@ from Algorithm.Algorithm import Algorithm
 class QuickSort(Algorithm):
 
     def sort(self, num_list):
-        pass
+
+        less_than = []
+        equals = []
+        greater_than = []
+
+        if len(num_list) > 1:
+            # there are actually numbers to sort
+
+            # setting the pivot
+            # using the first number as a pivot for simplicity
+            pivot = num_list[0]
+
+            # the actual sorting
+            for i in num_list:
+                if i < pivot:
+                    less_than.append(i)
+                if i == pivot:
+                    equals.append(i)
+                if i > pivot:
+                    greater_than.append(i)
+
+                # UPDATE THE VISUALIZATION WINDOW
+
+            return less_than + equals + greater_than
+
+        else:
+            # only 1 number
+            return num_list
