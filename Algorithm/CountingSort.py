1a6d4e4e13f5cd3b290df390f90ed385f6172067
Smallwoody <wksmallwod@gmail.com>
2018-03-26 12:50:26 -0500

    Counting Sort Text highlights

--- Algorithm/CountingSort.py
+++ Algorithm/CountingSort.py
@@ -20,8 +20,8 @@ class CountingSort(Algorithm):
         super().__init__(viz_window, cod_window)
 
         # setup code window
-        self.cod_window.set_alg_name("Bubble Sort")
-        self.cod_window.add_lines_from_file("Algorithm/BubbleSortCode.txt")
+        self.cod_window.set_alg_name("Counting Sort")
+        self.cod_window.add_lines_from_file("Algorithm/CountingSortCode.txt")
 
     def sort(self, num_list):
 
@@ -33,11 +33,53 @@ class CountingSort(Algorithm):
         m = max(num_list)
         count = [0] * m
         for a in range(n):
+
+            if self.highlight_enabled:
+                self.cod_window.highlight_line(0)
+            if self.steps_enabled:
+                wait_signal.wait()
+
             num_list[a] += 1
+
+            if self.highlight_enabled:
+                self.cod_window.highlight_line(1)
+            if self.steps_enabled:
+                wait_signal.wait()
+
         i = 0
+
+        if self.highlight_enabled:
+            self.cod_window.highlight_line(2)
+        if self.steps_enabled:
+
+            wait_signal.wait()
+
         for a in range(m):
+            if self.highlight_enabled:
+                self.cod_window.highlight_line(3)
+            if self.steps_enabled:
+
+                wait_signal.wait()
+
             for c in range(num_list[a]):
+                if self.highlight_enabled:
+                    self.cod_window.highlight_line(4)
+                if self.steps_enabled:
+                    wait_signal.wait()
+
                 num_list[i] = a
+
+                if self.highlight_enabled:
+                    self.cod_window.highlight_line(5)
+                if self.steps_enabled:
+                    wait_signal.wait()
+
                 i += 1
+
+                if self.highlight_enabled:
+                    self.cod_window.highlight_line(6)
+                if self.steps_enabled:
+                    wait_signal.wait()
+
         return num_list
 
