093c84a2d13a733be8db544c1dce9cefd06d0684
Smallwoody <wksmallwod@gmail.com>
2018-04-02 12:09:09 -0500

    All Sorts now have Code highlights

--- Algorithm/CountingSort.py
+++ Algorithm/CountingSort.py
@@ -11,9 +11,6 @@ from GUI.VisualizationWindow import VisualizationWindow
 class CountingSort(Algorithm):
 
     def __init__(self, viz_window: VisualizationWindow, cod_window: CodeHighlightWindow):
-        """
-        Initializes the Example Algorithm.
-        """
 
         # run the standard init
         super().__init__(viz_window, cod_window)
@@ -26,59 +23,55 @@ class CountingSort(Algorithm):
 
         if self.steps_enabled:
             wait_signal = Utils.WaitSignal(self.cod_window.signal_step)
+        maximum = max(num_list)
+        minimum = min(num_list)
+        count = [0]*(maximum-minimum+1)
 
-
-        n = len(num_list)
-        m = max(num_list)
-        count = [0] * m
-        for a in range(n):
+        for a in num_list:
 
             if self.highlight_enabled:
                 self.cod_window.highlight_line(0)
             if self.steps_enabled:
                 wait_signal.wait()
 
-            num_list[a] += 1
+            count[a-minimum] += 1
 
             if self.highlight_enabled:
                 self.cod_window.highlight_line(1)
             if self.steps_enabled:
                 wait_signal.wait()
 
-        i = 0
+        sorted = []
 
-        if self.highlight_enabled:
-            self.cod_window.highlight_line(2)
-        if self.steps_enabled:
-
-            wait_signal.wait()
-
-        for a in range(m):
+        for i in range(minimum, maximum+1):
             if self.highlight_enabled:
-                self.cod_window.highlight_line(3)
+                self.cod_window.highlight_line(2)
             if self.steps_enabled:
-
                 wait_signal.wait()
 
-            for c in range(num_list[a]):
+            if count[i-minimum] > 0:
+
                 if self.highlight_enabled:
-                    self.cod_window.highlight_line(4)
+                    self.cod_window.highlight_line(3)
                 if self.steps_enabled:
                     wait_signal.wait()
 
-                num_list[i] = a
+                for j in range(0, count[i-minimum]):
+
+                    if self.highlight_enabled:
+                        self.cod_window.highlight_line(4)
+                    if self.steps_enabled:
+                        wait_signal.wait()
+
+                    sorted.append(i)
 
                 if self.highlight_enabled:
                     self.cod_window.highlight_line(5)
                 if self.steps_enabled:
                     wait_signal.wait()
 
-                i += 1
 
-                if self.highlight_enabled:
-                    self.cod_window.highlight_line(6)
-                if self.steps_enabled:
-                    wait_signal.wait()
+        num_list = sorted
 
-        return num_list
+        return (num_list)
 
