23cbb9308c75fbcd4283586eb75523b3b472c3a6
Smallwoody <wksmallwod@gmail.com>
2018-04-03 16:42:11 -0500

    All Sorts now have Code highlights

--- Algorithm/CountingSort.py
+++ Algorithm/CountingSort.py
@@ -27,19 +27,34 @@ class CountingSort(Algorithm):
         minimum = min(num_list)
         count = [0]*(maximum-minimum+1)
 
+        if self.viz_enabled:
+            # create the graphs
+            self.viz_window.add_graph(g_index=0, name="Original")
+            self.viz_window.add_graph(g_index=1, name="Sorted")
+            self.viz_window.add_graph(g_index=2, name="Count")
+
+            # create the nodes
+            self.viz_window.add_nodes(num_list, g_index=0)
+            self.viz_window.add_nodes(count, g_index=2)
         for a in num_list:
 
             if self.highlight_enabled:
                 self.cod_window.highlight_line(0)
             if self.steps_enabled:
                 wait_signal.wait()
+            if self.delay:
+                Utils.sleep_qt(self.delay * 1000 / 3)
 
             count[a-minimum] += 1
+            if self.viz_enabled:
+                self.viz_window.set_node(a-minimum, count[a-minimum], g_index=2)
 
             if self.highlight_enabled:
                 self.cod_window.highlight_line(1)
             if self.steps_enabled:
                 wait_signal.wait()
+            if self.delay:
+                Utils.sleep_qt(self.delay * 1000 / 3)
 
         sorted = []
 
@@ -48,6 +63,10 @@ class CountingSort(Algorithm):
                 self.cod_window.highlight_line(2)
             if self.steps_enabled:
                 wait_signal.wait()
+            if self.delay:
+                Utils.sleep_qt(self.delay * 1000 / 3)
+
+
 
             if count[i-minimum] > 0:
 
@@ -55,21 +74,34 @@ class CountingSort(Algorithm):
                     self.cod_window.highlight_line(3)
                 if self.steps_enabled:
                     wait_signal.wait()
+                if self.delay:
+                    Utils.sleep_qt(self.delay * 1000 / 3)
 
                 for j in range(0, count[i-minimum]):
+                    if self.viz_enabled:
+                        k = num_list.index(i)
+                        self.viz_window.highlight_node(k, True, g_index=0)
+                    if self.delay:
+                        Utils.sleep_qt(self.delay * 1000 / 3)
 
                     if self.highlight_enabled:
                         self.cod_window.highlight_line(4)
                     if self.steps_enabled:
                         wait_signal.wait()
+                    if self.delay:
+                        Utils.sleep_qt(self.delay * 1000 / 3)
 
                     sorted.append(i)
-
-                if self.highlight_enabled:
-                    self.cod_window.highlight_line(5)
-                if self.steps_enabled:
-                    wait_signal.wait()
-
+                    if self.highlight_enabled:
+                        self.cod_window.highlight_line(5)
+                    if self.steps_enabled:
+                        wait_signal.wait()
+                    if self.viz_enabled:
+                        self.viz_window.add_node(i, g_index=1)
+                        k = num_list.index(i)
+                        self.viz_window.highlight_node(k, False, g_index=0)
+                    if self.delay:
+                        Utils.sleep_qt(self.delay * 1000 / 3)
 
         num_list = sorted
 
--- Algorithm/InsertionSort.py
+++ Algorithm/InsertionSort.py
@@ -12,9 +12,6 @@ from GUI.VisualizationWindow import VisualizationWindow
 class InsertionSort(Algorithm):
 
     def __init__(self, viz_window: VisualizationWindow, cod_window: CodeHighlightWindow):
-        """
-        Initializes the Example Algorithm.
-        """
 
         # run the standard init
         super().__init__(viz_window, cod_window)
@@ -24,6 +21,11 @@ class InsertionSort(Algorithm):
         self.cod_window.add_lines_from_file("Algorithm/InsertionSortCode.txt")
 
     def sort(self, num_list):
+
+        if self.viz_enabled:
+            # create the graphs
+            self.viz_window.add_graph(g_index=0, name="Original")
+            self.viz_window.add_nodes(num_list, g_index=0)
 
         if self.steps_enabled:
             wait_signal = Utils.WaitSignal(self.cod_window.signal_step)
@@ -35,22 +37,32 @@ class InsertionSort(Algorithm):
                 self.cod_window.highlight_line(0)
             if self.steps_enabled:
                 wait_signal.wait()
+            if self.delay:
+                Utils.sleep_qt(self.delay * 1000 / 3)
 
             # current_value variable refers to [i] in the array used for this algorithm
             current_value = num_list[i]
 
+            if self.viz_enabled:
+                self.viz_window.highlight_node(i, True)
             if self.highlight_enabled:
                 self.cod_window.highlight_line(1)
             if self.steps_enabled:
                 wait_signal.wait()
+            if self.delay:
+                Utils.sleep_qt(self.delay * 1000 / 3)
 
             # j is decremented of i
             j = i - 1
 
+            if self.viz_enabled:
+                self.viz_window.highlight_node(j, True)
             if self.highlight_enabled:
                 self.cod_window.highlight_line(2)
             if self.steps_enabled:
                 wait_signal.wait()
+            if self.delay:
+                Utils.sleep_qt(self.delay * 1000 / 3)
 
             # Move elements of num_list[0..i-1], that are
             # greater than current_value, to one position ahead
@@ -61,14 +73,24 @@ class InsertionSort(Algorithm):
                     self.cod_window.highlight_line(3)
                 if self.steps_enabled:
                     wait_signal.wait()
-
+                if self.delay:
+                    Utils.sleep_qt(self.delay * 1000 / 3)
+                if self.viz_enabled:
+                    self.viz_window.highlight_node(j, False)
                 # j is incremented
                 num_list[j + 1] = num_list[j]
-
+                if self.viz_enabled:
+                    self.viz_window.highlight_node(j, True)
+                    self.viz_window.highlight_node(j+1, True)
+                    self.viz_window.swap_nodes(j, j+1)
+                    self.viz_window.highlight_node(j, False)
+                    self.viz_window.highlight_node(j + 1, False)
                 if self.highlight_enabled:
                     self.cod_window.highlight_line(4)
                 if self.steps_enabled:
                     wait_signal.wait()
+                if self.delay:
+                    Utils.sleep_qt(self.delay * 1000 / 3)
 
                 # j is decremented
                 j -= 1
@@ -77,14 +99,20 @@ class InsertionSort(Algorithm):
                     self.cod_window.highlight_line(5)
                 if self.steps_enabled:
                     wait_signal.wait()
-
+                if self.delay:
+                    Utils.sleep_qt(self.delay * 1000 / 3)
             # j is incremented, j and current_value are set equal to each other
             num_list[j + 1] = current_value
-
+            if self.viz_enabled:
+                self.viz_window.highlight_node(j + 1, True)
+                self.viz_window.set_node(j+1,current_value)
+                self.viz_window.highlight_node(j + 1, False)
             if self.highlight_enabled:
                 self.cod_window.highlight_line(6)
             if self.steps_enabled:
                 wait_signal.wait()
+            if self.delay:
+                Utils.sleep_qt(self.delay * 1000 / 3)
 
         # return the sorted list
         return num_list
